758277079ac9ba9ad8163d5c0104814c1708d9ff
Rohan Sunil <reach.rohan0007@gmail.com>
2020-10-03 20:44:14 +0530

    Added : Singlet Combination

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -1,9 +1,26 @@
 #!/bin/bash -x
 echo "Welcome to flip coin Combination"
-random=$((RANDOM%2))
-if [[ $(($random)) -eq 0 ]]
-then
+
+read -p "Enter no. of times to flip coin " num
+T=0
+H=0
+i=0
+
+while [[ $i -lt num ]]
+do
+	random=$((RANDOM%2))
+	if [[ $(($random)) -eq 0 ]]
+	then
         echo "Head "
-else
+			((H++))
+	else
         echo "Tail "
-fi
+			((T++))
+	fi
+	((i++))
+done
+
+PercentageofH=$(( (H*100)/num ))
+PercentageofT=$(( (T*100)/num ))
+echo "Singlet combination of H and T are $H & $T"
+echo "% of head and tail are $PercentageofH % & $PercentageofT % "
