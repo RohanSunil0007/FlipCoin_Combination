788fca9a19d92401a5c1a58d0732a2bcb44e114c
Rohan Sunil <reach.rohan0007@gmail.com>
2020-10-03 21:33:45 +0530

    Added : Doublet Combination

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -2,25 +2,31 @@
 echo "Welcome to flip coin Combination"
 
 read -p "Enter no. of times to flip coin " num
-T=0
-H=0
+TT=0
+HH=0
 i=0
 
 while [[ $i -lt num ]]
 do
-	random=$((RANDOM%2))
-	if [[ $(($random)) -eq 0 ]]
+	random1=$((RANDOM%2))
+	random2=$((RANDOM%2))
+	if [[ $(($random1)) -eq 0 && $(($random2)) -eq 0 ]]
 	then
-        echo "Head "
-			((H++))
+        echo "Doublet Head "
+			((HH++))
+	elif [[ $(($random1)) -eq 1 && $(($random2)) -eq 1 ]]
+	then
+        echo "Doublet Tail "
+			((TT++))
 	else
-        echo "Tail "
+			echo "It is Tail and Head"
 			((T++))
+			((H++))
 	fi
 	((i++))
 done
 
-PercentageofH=$(( (H*100)/num ))
-PercentageofT=$(( (T*100)/num ))
-echo "Singlet combination of H and T are $H & $T"
-echo "% of head and tail are $PercentageofH % & $PercentageofT % "
+PercentageofHH=$(( (HH*100)/num ))
+PercentageofTT=$(( (TT*100)/num ))
+echo "Doublet combination of HH and TT are $HH & $TT"
+echo "% of Doublet head and Doublet tail are $PercentageofHH % & $PercentageofTT % "
